eb0d3f1376fb640d5496993c8714750d61e2d538
TeerapatEarth <earth_scorpion@hotmail.com>
2021-10-29 16:56:15 +0700

    fix upload image

--- backend/src/midleware/uploadImageProfile.js
+++ backend/src/midleware/uploadImageProfile.js
@@ -2,9 +2,9 @@ const multer = require("multer");
 const path = require("path");
 
 var storage = multer.diskStorage({
-  destination: function (req, file, callback) {
-    callback(null, "./src/static/imageUser"); // path to save file
-  },
+  // destination: function (req, file, callback) {
+  //   callback(null, "./src/static/imageUser"); // path to save file
+  // },
   filename: function (req, file, callback) {
     // set file name
     callback(
